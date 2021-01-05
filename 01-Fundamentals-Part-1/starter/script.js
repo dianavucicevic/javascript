421cb481646c7c68f7f9eee5dfef88904c695f4c
Diana <74531160+dianavucicevic@users.noreply.github.com>
2021-01-05 16:15:11 +0100

    05.01.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,59 @@
+/*let js = "amazing";
+if (js === "amazing") alert ("Javascript is FUN!");
+
 console.log(10 + 20);
 let firstName = "Jonas";
 console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'John');
+
+javaScriptIsFun = "YES";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+*/
+
+/*let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;
+
+var job = "teacher" //old way of declaring a variable */
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2**3);
+// 2 ** 3 means 2*2*2
+
+const firstName = "Jonas";
+const lastName = "Doe";
+console.log(firstName + " " + lastName);
+
+
+
+
+
+
+
+
 
