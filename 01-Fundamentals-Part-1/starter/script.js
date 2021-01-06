e8cef4dd78028adbccf0f050e1b820857040e80e
Diana <74531160+dianavucicevic@users.noreply.github.com>
2021-01-06 16:21:32 +0100

    06.01.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -34,13 +34,9 @@ age = 31;
 const birthYear = 1991;
 birthYear = 1990;
 
-var job = "teacher" //old way of declaring a variable */
+var job = "teacher" //old way of declaring a variable 
 
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2**3);
 // 2 ** 3 means 2*2*2
@@ -48,6 +44,78 @@ console.log(ageJonas * 2, ageJonas / 10, 2**3);
 const firstName = "Jonas";
 const lastName = "Doe";
 console.log(firstName + " " + lastName);
+
+//Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x+10 = 25
+x *= 4; // x = x* 4 = 100
+x++; // x = x + 1
+x--;
+x--; // x = x - 1 = 99
+
+//Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >=18;
+
+
+
+//Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(now - 1991 > now - 2018);
+
+let x,y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2 // division has an advantage over addition
+console.log(ageJonas, ageSarah, averageAge);
+
+//coding challenge 1
+
+const marksWeight = 78;
+const marksHeight = 1.69;
+
+const johnsWeight = 92;
+const jonhnsHeight = 1.95;
+
+const marksBmi = marksWeight / (marksHeight ** 2);
+const johnsBmi = johnsWeight / (jonhnsHeight ** 2);
+console.log(marksBmi, johnsBmi);
+
+const markHigherBmi = marksBmi > johnsBmi;
+
+console.log(markHigherBmi);
+*/
+
+const firstName = "Diana";
+const job = "tester";
+const birthYear = 1992;
+const year = 2037;
+
+const diana = "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(diana);
+
+const dianaNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(dianaNew);
+
+console.log(`Multiline
+string
+!`) //when using `` we can create multiline string by pressing enter
+
+
+
+
+
+
+
+
+
 
 
 
