d02b549c93cc08a6db4e1b7edf68c56a8eab4b75
Diana <74531160+dianavucicevic@users.noreply.github.com>
2021-01-13 14:43:04 +0100

    13.01.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,8 +1,26 @@
-'use strict';
+/*'use strict';
 
-/*let hasDriversLicense = false;
+let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriverLicense = true;
 if (hasDriversLicense) console.log("I can drive.");*/
 
+function logger() {
+    console.log("My name is Diana");
+}
+
+logger();
+
+
+function fruitProcessor (apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor (2,3);
+console.log(appleOrangeJuice);
